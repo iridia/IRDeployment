fc0dae15380508bfd2c0e46294041c1b5ae385dd
Evadne Wu <ev@monoceroi.com>
2012-01-20 22:01:53 +0800

    more strict with errors

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,24 @@
 #!/bin/bash --
 
-source "`dirname $0`/environment.sh"
+ENV_FILE="`dirname $0`/environment.sh"
+if [ ! -f $ENV_FILE ]; then
+    echo "environment.sh not found.  Consult environment-template.sh."
+	exit 1
+fi
+source $ENV_FILE
+
+function die () {
+	echo "$1"; exit
+}
+
+function bailIfError () {
+	if [ $? -ne 0 ]; then die "☠ Bad stuff happened"; fi
+}
+
 
 TEMP_DIR=`mktemp -d '/tmp/Etoile.XXXXXX'`	# Clean me up
 
-xcodebuild clean build -configuration $BUILD_CONFIGURATION -sdk $BUILD_SDK CODE_SIGN_IDENTITY="$CODE_SIGN_IDENTITY" SYMROOT="$TEMP_DIR" PROVISIONING_PROFILE="$PROVISIONING_PROFILE"
+xcodebuild clean build -configuration $BUILD_CONFIGURATION -sdk $BUILD_SDK CODE_SIGN_IDENTITY="$CODE_SIGN_IDENTITY" SYMROOT="$TEMP_DIR" PROVISIONING_PROFILE="$PROVISIONING_PROFILE"; bailIfError
 
 cd "$TEMP_DIR/$BUILD_CONFIGURATION-$BUILD_SDK"
 mkdir Payload
@@ -13,7 +27,6 @@ cp -r $PRODUCT_NAME Payload
 zip -qr $IPA_NAME Payload
 zip -qr $DSYM_ZIP_NAME $DSYM_NAME
 
-curl $TF_API_URI -F file=@"$IPA_NAME" -F dsym=@"$DSYM_ZIP_NAME" -F api_token="$TF_API_TOKEN" -F team_token="$TF_TEAM_TOKEN" -F notes="$TF_NOTES" -F notify="$TF_NOTIFY"
+curl $TF_API_URI -F file=@"$IPA_NAME" -F dsym=@"$DSYM_ZIP_NAME" -F api_token="$TF_API_TOKEN" -F team_token="$TF_TEAM_TOKEN" -F notes="$TF_NOTES" -F notify="$TF_NOTIFY"; bailIfError
 
 rm -rv $TEMP_DIR
-
